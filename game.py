2129f259125f89b8d116fa1827e082b82f66d2fd
david <davidally94@gmail.com>
2019-03-19 23:43:32 -0400

    Function typo

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ class Dye(object):
     def __init__(self, sides):
         self.sides = [i + 1 for i in range(sides)]
 
-    def roll_dye(self):
+    def roll(self):
         return random.choice(self.sides)
 
 
@@ -32,7 +32,6 @@ class PigGameInstance(object):
         self.player_data = {}
         self.pending_points = 0
         self.current_player_turn = None
-        self.player_iter = iter(self.player_data)
 
     def add_player(self, user):
         self.player_data[user.name] = user.player_score
@@ -42,6 +41,7 @@ class PigGameInstance(object):
             if player[1] >= 100:
                 print '{} has won the game! \n Would you like to play again?'.format(
                     player[0])
+                self.reset_state()
                 return
 
     def display_scores(self):
@@ -56,9 +56,10 @@ class PigGameInstance(object):
         self.player_data.clear()
         self.pending_points = 0
 
+    def get_current_player(self):
+        return self.current_player_turn
+
     def player_turn(self):
-        # Cycle through players
-        self.player_iter.next()
         # Get input and validate
         player_response = raw_input('Will you Roll or Hold?')
         if not re.match(r'(roll|hold|r|h)', player_response, flags=re.IGNORECASE):
@@ -70,10 +71,34 @@ def main():
 
     # Initialize game
     pig_game = PigGameInstance()
+    game_dye = Dye(6)
+    player_rotate = iter(pig_game.player_data)
 
+    # Set up the match
     player_count = raw_input('Set amount of players for this game: ')
     for _ in range(int(player_count)):
         pig_game.add_player(User())
+
+    while pig_game:
+        pig_game.current_player_turn = next(player_rotate)
+        curr_player = pig_game.current_player_turn
+        bad_roll = False
+
+        while bad_roll == False:
+            response = pig_game.player_turn()
+            if response == 'r' or response == 'roll':
+                current_roll = game_dye.roll()
+                if current_roll == 1:
+                    bad_roll = True
+                    continue
+                else:
+                    pig_game.pending_points += current_roll
+                    pig_game.player_data[curr_player] += pig_game.pending_points
+                    print curr_player
+                    pig_game.display_scores()
+                    pig_game.check_if_winner()
+            else:
+                continue
 
     pig_game.display_scores()
 
