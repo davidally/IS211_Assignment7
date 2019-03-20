4133be18f43bc5f0123566ad93f822adaadea6c1
david <davidally94@gmail.com>
2019-03-19 22:41:48 -0400

    Typo fix

--- game.py
+++ game.py
@@ -3,14 +3,13 @@
 
 import random
 import argparse
+import re
 
 
 class User(object):
 
-    def __init__(self, player_name):
-        # if not isinstance(player_name, basestring):
-        #     raise ValueError('Please input a proper name.')
-        self.name = player_name
+    def __init__(self):
+        self.name = raw_input('Enter a player name: ')
         self.player_score = 0
 
     def add_points(self, points):
@@ -33,12 +32,17 @@ class PigGameInstance(object):
         self.player_data = {}
         self.pending_points = 0
         self.current_player_turn = None
+        self.player_iter = iter(self.player_data)
 
     def add_player(self, user):
         self.player_data[user.name] = user.player_score
 
-    def get_highest_score(self):
-        print max(self.player_data, key=self.player_data.get)
+    def check_if_winner(self):
+        for player in self.player_data:
+            if player[1] >= 100:
+                print '{} has won the game! \n Would you like to play again?'.format(
+                    player[0])
+                return
 
     def display_scores(self):
         # Format the dictionary entries and join the list to the string
@@ -52,14 +56,24 @@ class PigGameInstance(object):
         self.player_data.clear()
         self.pending_points = 0
 
+    def player_turn(self):
+        # Cycle through players
+        self.player_iter.next()
+        # Get input and validate
+        player_response = raw_input('Will you Roll or Hold?')
+        if not re.match(r'(roll|hold|r|h)', player_response, flags=re.IGNORECASE):
+            raise ValueError('Please enter one of the two valid responses.')
+        return player_response
+
 
 def main():
 
     # Initialize game
     pig_game = PigGameInstance()
 
-    pig_game.add_player(User('Ronald McJenkins'))
-    pig_game.add_player(User('Tony Toucan'))
+    player_count = raw_input('Set amount of players for this game: ')
+    for _ in range(int(player_count)):
+        pig_game.add_player(User())
 
     pig_game.display_scores()
 
